acf96eb0cfa7a9794713f657ca5f1c742278d67d
e.akhantev <e.akhantev@s7.ru>
2024-03-18 14:01:21 +0400

    feat: complete task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -221,8 +221,19 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const dateStart = new Date(date.getTime());
+  const infiteCondition = true;
+  let currentDate = dateStart.getDate();
+  let currentDay = dateStart.getUTCDay();
+
+  while (infiteCondition) {
+    if (currentDate === 13 && currentDay === 4) break;
+    dateStart.setDate(dateStart.getDate() + 1);
+    currentDate = dateStart.getDate();
+    currentDay = dateStart.getUTCDay();
+  }
+  return dateStart;
 }
 
 /**
